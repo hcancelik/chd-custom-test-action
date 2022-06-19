ea2b36a0dd281e2fcfc634915cae99c0cd859ecb
H. Can Celik <celik@me.com>
2022-06-19 19:57:56 +0300

    Wip

--- dist/index.js
+++ dist/index.js
@@ -10257,7 +10257,7 @@ async function checkComment (token) {
 async function addComment (token, comment) {
   const octokit = getOctokit(token);
   const { owner, repo } = context.repo;
-  const { data } = await octokit.rest.repos.createCommitComment({
+  const { data } = await octokit.rest.repos.createComment({
     owner,
     repo,
     issue_number: prNumber,
